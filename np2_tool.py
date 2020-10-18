b53f790d08957ccfa4b28a61d2de29aac64c0a5d
Adam Phelps <366518+aphelps@users.noreply.github.com>
2020-10-18 00:29:21 -0700

    Got upgrades working, more star functions

--- np2_tool.py
+++ np2_tool.py
@@ -8,6 +8,17 @@ import pickle
 import requests
 import sys
 
+################################################################################
+# Constants
+#
+
+# 3 hours per LY
+LIGHT_YEAR_TIME = 3
+
+# 1 star scale = 8 light years observed
+# docs say 1/16, but seem to be wrong
+LIGHT_YEAR_SCALE = 8
+
 
 def handle_args():
     parser = argparse.ArgumentParser()
@@ -52,10 +63,24 @@ def load_cookies(login, password, credentials):
 
 
 def get_universe(cookies, game_id):
-    universe = requests.post('https://np.ironhelmet.com/trequest/order',
-        data = {'type':'order', 'order':'full_universe_report', 'version':'', 'game_number':'5380395345117184'},
-        cookies=cookies).json()
+    response = requests.post('https://np.ironhelmet.com/trequest/order',
+                             data={'type':'order',
+                                   'order':'full_universe_report',
+                                   'version':'',
+                                   'game_number':'5380395345117184'},
+                             cookies=cookies)
     # print("Universe: %s" % json.dumps(universe))
+
+    universe = response.json()
+    if response.status_code != requests.codes.ok:
+        print("get_universe(): request failed, code %d" % response.status_code)
+        print("  data: %s" % response.text)
+        sys.exit(1)
+
+    if 'player_uid' not in universe['report']:
+        print("get_universe(): invalid universe: %s" % (json.dumps(universe)))
+        sys.exit()
+
     with open("universe.json", "w") as f:
         f.write(json.dumps(universe))
     return universe
@@ -68,23 +93,24 @@ def player_stars(universe, player_id):
             stars.append(star)
     return sorted(stars, key = lambda i: i['n'])
 
-'''
-    universe.calcUCE = function (star) {
-        if (star.player !== universe.player) return 0;
-        let e = star.e + 1;
-        return Math.floor((10 * e * e) / (star.r / 100));
-    };
-    universe.calcUCI = function (star) {
-        if (star.player !== universe.player) return 0;
-        let i = star.i + 1;
-        return Math.floor((15 * i * i) / (star.r / 100));
-    };
-    universe.calcUCS = function (star) {
-        if (star.player !== universe.player) return 0;
-        let s = star.s + 1;
-        return Math.floor((20 * s * s) / (star.r / 100));
-    };
-'''
+
+# Star format:
+#     {
+#         u'e': 4,             # Economy
+#         u'uid': 294,         # Star ID
+#         u'i': 3,             # Industry
+#         u'puid': 5,          # Player ID
+#         u'n': u'Sham',       # Name
+#         u's': 2,             # Science
+#         u'r': 40,            # Resources
+#         u'exp': 0,
+#         u'v': u'1',
+#         u'y': u'5.04934006', # y location
+#         u'x': u'0.69888656', # x location
+#         u'ga': 0,
+#         u'st': 290           # Ships
+#     }
+
 def star_upgrade_costs(star):
     e = star['e'] + 1
     i = star['i'] + 1
@@ -96,10 +122,43 @@ def star_upgrade_costs(star):
     }
 
 
+def star_by_name(stars, name):
+    return next(star for star in stars if star["n"] == name)
+
+
+def star_by_id(stars, id):
+    return next(star for star in stars if star["uid"] == id)
+
+
 def star_upgrades(stars):
     for star in stars:
         upgrades = star_upgrade_costs(star)
-        print("%s: e:%d i:%d s:%d" % (star['n'], upgrades["economy"], upgrades["industry"], upgrades["science"]))
+        print("%s[%s]: e:%d i:%d s:%d" % (star['n'], star['uid'], upgrades["economy"], upgrades["industry"], upgrades["science"]))
+
+
+def star_distance(star1, star2):
+    distance = math.sqrt(
+        (float(star1['x']) - float(star2['x']))**2 +
+        (float(star1['y']) - float(star2['y']))**2)
+    return {
+        'distance': distance * LIGHT_YEAR_SCALE,
+        'time': math.ceil(distance * LIGHT_YEAR_SCALE * LIGHT_YEAR_TIME)
+    }
+
+
+def star_upgrade(cookies, game_id, star, type):
+    id = star['uid']
+    amount = star_upgrade_costs(star)['industry']
+    data = {'type': 'batched_orders',
+            'order': 'upgrade_%s,%d,%d' % (type, id, amount),
+            'version': '',
+            'game_number': '%d' % game_id}
+    print("TEST: %s" % data)
+
+    result = requests.post('https://np.ironhelmet.com/prequest/batched_orders',
+                           data=data,
+                           cookies=cookies)
+    print('test_upgrade(): status=%d text=%s' % (result.status_code, result.text))
 
 
 def main():
@@ -118,6 +177,9 @@ def main():
 
     star_upgrades(stars)
 
+    # test_upgrade(cookies, options.gameid, star_by_name(stars, "Kochab"), "industry")
+
+    print('distance: %s' % star_distance(star_by_name(stars, "Alnilam"), star_by_name(stars, "Septen")))
 
 if __name__ == '__main__':
     main()
